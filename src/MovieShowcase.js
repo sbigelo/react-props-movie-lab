f1c02fdb26d24eb02de2c9a10195f08708cfb252
sbigelo <sean.bigelow1@gmail.com>
2021-11-11 13:46:23 -0600

    commit tree 875cd918d9aa49833131c5d744fce4629e5cfdc6

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,6 +6,10 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+  //  const allData = movieData.map(data => <MovieCard title={data.title} IMDBRating={data.IMDBRating} />)
+
+    return movieData.map((data, index) => <MovieCard key={index} title={data.title} IMDBRating={data.IMDBRating} genres={data.genres} poster={data.poster}/>)
+   
   }
 
   render() {
--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -5,6 +5,7 @@ export default class CardFront extends Component {
   render() {
     return (
       <div className="card-front" style={{backgroundImage: `url(${''})`}}>
+        {this.props.poster}
       </div>
     )
   }
